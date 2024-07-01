ca1ee69b550632f73610a676addd1a65ec0ea634
Sina Dabiri <35987747+Simurgh818@users.noreply.github.com>
2024-07-01 11:56:44 -0400

    first commit.

---
+++ GED_practice.m
@@ -0,0 +1,13 @@
+%%
+% Generalized Eigendecomposition (GED) Practice
+%
+%
+% Sina Dabiri (sdabiri@emory.edu)
+%% Preprocessing
+% 1- epoching? 30 sec ?
+% 2- Bandpass filtering: 2-300 Hz?
+% 3- Baseline correction: high pass filtering to remove DC shift
+% 4- GED calculation
+% 5- Visualization and plotting
+
+
