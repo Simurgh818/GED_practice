1ccb7981e755c655ec60367f4575b3074a5c3e57
Sina Dabiri <35987747+Simurgh818@users.noreply.github.com>
2024-07-03 12:03:50 -0400

    plotting the raw channel 1, 2Rd2

--- GED_practice.m
+++ GED_practice.m
@@ -17,7 +17,25 @@ close all; clear, clc
 %define root dir (where project data is) and sessions of interest:
 root_dir='C:\Users\sinad\OneDrive - Emory University\Dr.Drane\Cognitive Mapping\Subject01';
 file_path = fullfile(root_dir, "Subject_001-RestingState.EDF");
-seeg_data = edfread(file_path);
-disp(seeg_data)
+[seeg_info, seeg_data] = edfread(file_path);
+% disp(seeg_info)
 
-% params.Fs = seeg_data.Duration
+params.Fs = seeg_info.samples(1)/seeg_info.duration;
+nCh = seeg_info.ns;
+time_seg = 30; %for a 10 sec segment
+
+Num_samples=time_seg*params.Fs;
+temp_data = zeros(round(Num_samples),1);
+for i=1:Num_samples+1
+        temp_data(i)=seeg_data(1,i+round(time_seg*params.Fs));
+end
+
+T = 1/params.Fs;
+[L,~] = size(temp_data);
+t = (0:L-1)*T(1);
+figure(1)
+subplot(2,2,1)
+plot(t,temp_data)
+xlabel("time (sec)")
+ylabel("Amp (uV)")
+title(seeg_info.label(1)+" raw")
